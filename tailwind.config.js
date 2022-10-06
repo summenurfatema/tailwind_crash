5e5f821ade82fb96527d1ed0dcd186e299724184
Fatema04 <shadrinmoni15@gmail.com>
2022-10-05 23:41:54 -0700

    nav box added

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,13 @@ module.exports = {
       },
       colors: {
         brightRed: 'hsl(12,88%,59%)',
-        brightRedLight: 'hsl(12,88%,69%   )',
-        brightRedSuperLight: 'hsl(12, 88%,95%   )',
-        darkBlue: 'hsl( 228,39%,23%  )',
-        darkGrayisBlue: 'hsl( 227,12%,61%  )',
-        veryDarkBlue: 'hsl(233,12%,13%   )',
-        veryPaleRed: 'hsl( 13,100%,96%  )',
-        varyLightGray: 'hsl(0,0%,98%  )'
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSuperLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl( 228, 39%, 23%)',
+        darkGrayisBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13% )',
+        veryPaleRed: 'hsl(13, 100%, 96% )',
+        varyLightGray: 'hsl(0, 0%, 98% )'
 
       }
 
